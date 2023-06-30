6a364771e711d94b1420df2759b63df01884f25d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-06-30 17:30:09 +0900

    Initial Commit

---
+++ SeMACollections/SeMACollectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeMACollectionsApp.swift
+//  SeMACollections
+//
+//  Created by Jinwook Kim on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct SeMACollectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
